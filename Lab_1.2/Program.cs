4b9e47d582745f57b7d4f52c5c303744329a0c28
MaxMartST <AdderGtx@gmail.com>
2025-09-21 19:46:09 +0300

    Лабораторная 1.2

---
+++ Lab_1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿static void PrintA()
+{
+    for (int i = 1; i <= 100; i++)
+        Console.WriteLine($"Thread {Thread.CurrentThread.Name} : {i}");
+}
+static void PrintB(object? p)
+{
+    if (p is Thread t)
+    {
+        while (t.ThreadState == ThreadState.Unstarted) 
+            Thread.Yield();
+
+        t.Join();
+    }
+
+    for (int i = 1; i <= 100; i++)
+        Console.WriteLine($"Thread {Thread.CurrentThread.Name} : {i}");
+}
+
+Thread t0 = new Thread(PrintA) { Name = "A" };
+Thread t1 = new Thread(PrintB) { Name = "B" };
+
+t1.Start(t0);
+Thread.Sleep(1000);
+t0.Start();
