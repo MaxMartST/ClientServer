f4f98eef4dc4b6bcd1d3a68c3329617e7c44365f
MaxMartST <AdderGtx@gmail.com>
2025-09-21 19:57:25 +0300

    Лабораторная 1.3

---
+++ Lab_1.3/Program.cs
@@ -0,0 +1,50 @@
+﻿class Sync
+{
+    public double x = 1;
+    public bool phase = true;
+
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Sync s = new Sync();
+
+        new Thread(() => {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (s)
+                {
+                    while (!s.phase)
+                        Monitor.Wait(s);
+
+                    s.x = Math.Cos(s.x);
+                    Console.Write($"{s.x}  ");
+
+                    s.phase = !s.phase;
+                    Monitor.Pulse(s); // PulseAll
+                }
+            }
+
+        }).Start();
+
+        new Thread(() => {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (s)
+                {
+                    while (s.phase)
+                        Monitor.Wait(s);
+
+                    s.x = Math.Acos(s.x);
+                    Console.WriteLine($"{s.x}");
+
+                    s.phase = !s.phase;
+                    Monitor.Pulse(s); // PulseAll
+                }
+            }
+
+        }).Start();
+    }
+}
