5c7ab3d852703fa6402f3cabc2be6ee36d540ce2
MaxMartST <AdderGtx@gmail.com>
2025-09-21 18:05:21 +0300

    Лабораторная 1.1

--- ClientServer/Program.cs
+++ ClientServer/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿record class MyThread
+{
+    public string NameThread { get; init; } = "Print_10";
+    public int Start { get; init; } = 1;
+    public int End { get; init; } = 10;
+
+    public void Run()
+    {
+        Thread.CurrentThread.Name = NameThread;
+
+        for (int i = Start; i <= End; i++)
+            Console.WriteLine($"Thread {Thread.CurrentThread.Name} : {i}");
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Thread t0 = new Thread(new MyThread().Run);
+        Thread t1 = new Thread(new MyThread { NameThread = "A", Start = 6, End = 12 }.Run);
+
+        t0.Start();
+        t1.Start();
+    }
+}
